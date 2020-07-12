d07f10f86769fe979bedfaaca9e4ffe47761feb9
damo <hemoparrot@gmail.com>
2020-07-12 11:38:23 +0800

    修改main.py

--- xdopenhard/main.py
+++ xdopenhard/main.py
@@ -9,15 +9,21 @@ from github import Github
 import pandas as pd
 import numpy as np
 import re
+import git
 
 df=pd.read_excel('data/data.xlsx',sheet_name='Sheet1',header=1,index=False)
 
 df=df.drop_duplicates(subset=['姓名'],keep='last')
+count1=0
+count2=0
 for i,row in df.iterrows():
-    if(re.search("^git.*git$",row['项目地址'])):
+    if(re.search("^git.*git$",row['仓库地址'])):
+        print(str(row['序号'])+row['姓名']+':'+row['仓库地址']+' OK')
+        repo=git.Repo.clone_from(url=row['仓库地址'],to_path=row['姓名'])        
+        count1=count1+1
         continue 
     else:
-        print(row['姓名']+':'+row['项目地址'])
-
-
-
+        print(str(row['序号'])+row['姓名']+':'+row['仓库地址'])
+        count2=count2+1
+print('合格:', count1,'人')
+print('不合格:', count2,'人')
