d3bf3a2f7f2b07fc8af9b8464b725b49b403f961
damo wang <hemoparrot@gmail.com>
2024-05-09 10:54:13 +0800

    add AI

---
+++ arduino/code/arduino_strc/arduino.h
@@ -0,0 +1,5 @@
+#ifndef __ARDUINO_H
+#define __ARDUINO_H
+void setup();
+void loop();
+#endif /*__ARDUINO_H*/
---
+++ arduino/code/arduino_strc/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "arduino.h"
+int main()
+{
+    setup();
+    while(1)
+    {
+        loop();
+    }
+    return 0;
+}
